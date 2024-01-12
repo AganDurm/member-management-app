dcceec303a2c5b5c65840dd6d967717c7d35c54d
AganDurm <DurmisevicAgan@outlook.de>
2024-01-12 06:24:44 +0100

    refactoring

--- frontend-app/src/app/model/changed-members-data.ts
+++
@@ -1,9 +0,0 @@
-export class ChangedMembersData {
-    valueToUpdate: string;
-    userId: number;
-
-    constructor(valueToUpdate: string, userId: number) {
-        this.valueToUpdate = valueToUpdate;
-        this.userId = userId;
-    }
-}

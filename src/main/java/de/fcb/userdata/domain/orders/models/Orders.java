8cd02adaddb19aae8f16479b51303ce79f03fdfb
AganDurm <DurmisevicAgan@outlook.de>
2024-01-12 17:32:22 +0100

    refactoring

--- src/main/java/de/fcb/userdata/domain/orders/models/Orders.java
+++ src/main/java/de/fcb/userdata/domain/orders/models/Orders.java
@@ -1,7 +1,6 @@
 package de.fcb.userdata.domain.orders.models;
 
 import jakarta.annotation.Nonnull;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
